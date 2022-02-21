d48854ac0bb689151bb2a5ace56e080789d030ce
Simon <simon.web999@gmail.com>
2022-02-21 08:40:13 +0100

    added api upload route

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,12 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-
+router.post("/create_order", (req, res) => {
+    res.status(100)
+})
+router.get("/get_order", (req, res) => {
+    res.status(100)
+})
 
 
 module.exports = router;
