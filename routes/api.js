887a8355a165a2b81f828ccd9af59327b969ea45
Simon <simon.web999@gmail.com>
2022-03-07 10:05:57 +0100

    DB connection now working

--- routes/api.js
+++ routes/api.js
@@ -3,21 +3,23 @@ const router = express.Router();
 const mysql = require('mysql');
 
 
-// const con = mysql.createConnection({
-//     host: "devel1",
-//     user: "Carlos",
-//     password: "carlos"
-// });
+const con = mysql.createConnection({
+    host: "devel1",
+    user: "admin",
+    password: "the27",
+    database: "Carlos"
+});
 
-// con.connect(function (err) {
-//     if (err) console.log(err)
-//     console.log("Connected!")
-// });
+con.connect(function (err) {
+    if (err) console.log(err)
+    else console.log("Connected!")
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+
 
 router.post("/order", (req, res) => {
     //TODO use statuses
