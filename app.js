854c461892dbd54d2e80867b25b716006db58e3f
Weber Simon <simon.weber@edvschule-plattling.de>
2022-07-06 07:16:35 +0000

    Doc (not finished)

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/order');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/order');
 const con = require("./objects/DBconnection");
-var cors = require('cors')
+const cors = require('cors');
 
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,13 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 
-global.repairQuery = function (sqlQuery) {
-    return JSON.parse(JSON.stringify(sqlQuery));
+global.repairJson = function (json) {
+    return JSON.parse(JSON.stringify(json));
 }
+
 global.checktoken = async function (token) {
     return new Promise((resolve, reject) => {
         con.query('select UNIX_TIMESTAMP(expiration_date) ed from `Users` where  API_TOKEN = ?', [token], (error, result) => resolve(
-            (repairQuery(result)[0] && new Date(repairQuery(result)[0]["ed"] * 1000).getTime() > Date.now())
+            (repairJson(result)[0] && new Date(repairJson(result)[0]["ed"] * 1000).getTime() > Date.now())
         ))
     })
 }
